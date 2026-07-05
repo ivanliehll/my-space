67298f67d4327e100354111754eb6ac6d290af0f
ivanliehll <ivanliehll@gmail.com>
2026-07-05 18:15:47 +0800

    fix: 修正 siteURL 設定以修復 CSS 路徑錯亂

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -6,7 +6,7 @@ const SITE_LANG = "en"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 export const siteConfig: SiteConfig = {
 	title: "星奈的個人基地",
 	subtitle: "An Automatic and anime lover's personal website ",
-	siteURL: "https://effulgent-brigadeiros-24d0c4.netlify.app/", // 请替换为你的站点URL，以斜杠结尾
+	siteURL: "https://ivanliehll.github.io", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2026-07-04", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 	lang: SITE_LANG,
